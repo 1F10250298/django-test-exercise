de2867723777cd456d0419f324ad00d09c7256c4
1F10250259 <s1f102502597@iniad.org>
2026-07-02 12:05:06 +0900

    終了機能を追加しました。

--- todo/views.py
+++ todo/views.py
@@ -33,3 +33,13 @@ def detail(request, task_id):
         'task': task,
     }
     return render(request, 'todo/detail.html', context)
+
+
+def close(request, task_id):
+    try:
+        task = Task.objects.get(pk=task_id)
+    except Task.DoesNotExist:
+        raise Http404("Task does not exist")
+    task.comleted = True
+    task.save()
+    return redirect(index)
